ec4b4cb0374f8343ac95eb5dc470bc7c76e8bc13
Tonizhan325 <angelzhan666@gmail.com>
2025-10-01 20:42:51 +0200

    Método MostrarDatos() creada

--- PF01ACTIVIDAD1/PF01ACTIVIDAD1/Producto.cs
+++ PF01ACTIVIDAD1/PF01ACTIVIDAD1/Producto.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PF01ACTIVIDAD1
+﻿namespace PF01ACTIVIDAD1
 {
-    internal class Producto
+    class Producto
     {
+        public string Nombre;
+        public double Precio;
+        public bool EnStock;
+        public string Descripcion;
+
+        public Producto(string nombre, double precio, bool enStock, string descripcion)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            EnStock = enStock;
+            Descripcion = descripcion;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Precio: " + Precio);
+            Console.WriteLine("En Stock: " + EnStock);
+            Console.WriteLine("Descripcion: " + Descripcion);
+        }
     }
 }
