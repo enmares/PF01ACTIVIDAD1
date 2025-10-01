7428e4f690e2cf8a14be4c87deca9f6ac11ff155
enmares <enmares2000@gmail.com>
2025-10-01 20:36:05 +0200

    Añadidos atributos y Clase Persona, junto con constructor

---
+++ PF01ACTIVIDAD1/PF01ACTIVIDAD1/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF01ACTIVIDAD1
+{
+    public enum EstadoCivil
+    {
+        SOLTERO,
+        CASADO
+    }
+    public class Persona
+    {
+        public string Nombre;
+        public int Edad;
+        public EstadoCivil EstadoCivil;
+
+        public Persona(string nombre, int edad, EstadoCivil eCivil)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            EstadoCivil = eCivil;
+        }
+    }
+}
