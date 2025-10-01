8dc7d04424ae115d43a94515ca62cc188e46940d
enmares <enmares2000@gmail.com>
2025-10-01 20:12:28 +0200

    1r commit

---
+++ PF01ACTIVIDAD1/PF01ACTIVIDAD1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PF01ACTIVIDAD1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
